60342e0135e0427785b455df1ee6d84ccd348d25
Hydradominatus <62065673+Hydradominatus@users.noreply.github.com>
2020-09-28 17:49:43 +0300

    Test first ADD
    123

---
+++ LS2/js/app.js
@@ -0,0 +1,19 @@
+// let i = 2
+// let x
+// let y = 1
+// let z = 2**53 - 1
+
+// while(i<999){
+//     const x = (y*i)
+//     console.log(BigInt(x))
+//     y = x;
+//     i++;
+// }
+// console.log(x)
+let i
+for (let i = 0; i <= 1000; i=i+10) {
+
+    alert(i);
+
+  }
+console.log(i)
